dfbf63b1e4e84623f31003169c76da4a6ce6f2d8
github username <github email address>
2017-08-13 22:45:26 -0700

    soln seems more clever than practical

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -21,3 +21,12 @@ def oxford_comma(array)
     "#{array.slice(0, array.length-1).join(', ')}, and #{array.slice(-1)}"
   end
 end
+
+def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
+end
